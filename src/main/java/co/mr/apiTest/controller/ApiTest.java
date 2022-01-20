df471706b440eb26887eb69f42bedbd8a2055968
mrofftin <mrofftin@gmail.com>
2022-01-21 05:32:55 +0900

    공공데이터 API TEST - 타임리프적용

--- src/main/java/co/mr/apiTest/controller/ApiTest.java
+++ src/main/java/co/mr/apiTest/controller/ApiTest.java
@@ -2,6 +2,8 @@ package co.mr.apiTest.controller;
 
 import co.mr.apiTest.model.TourInfo;
 import com.google.gson.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +14,14 @@ import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
-@RestController
+@Controller
 public class ApiTest {
 
     @GetMapping("/apiTest")
-    public String apiTest() throws IOException {
-        String date ="2022011010";
+    public String apiTest(Model model) throws IOException{
+        String date = "2022011010";
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/TourStnInfoService/getTourStnVilageFcst"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=NgM4AEfqR5lvPHpMnDdOqa1EgpcRUSBeiKmvLmo3RyleWNOBGKNWS%2FNPLpkP12MlI6GGER6keJzILgL2dnuUuQ%3D%3D"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
@@ -27,6 +30,7 @@ public class ApiTest {
         urlBuilder.append("&" + URLEncoder.encode("CURRENT_DATE", "UTF-8") + "=" + URLEncoder.encode(date, "UTF-8")); /*2016-12-01 01시부터 조회*/
         urlBuilder.append("&" + URLEncoder.encode("HOUR", "UTF-8") + "=" + URLEncoder.encode("24", "UTF-8")); /*CURRENT_DATE부터 24시간 후까지의 자료 호출*/
         urlBuilder.append("&" + URLEncoder.encode("COURSE_ID", "UTF-8") + "=" + URLEncoder.encode("341", "UTF-8")); /*관광 코스ID*/
+
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -45,11 +49,13 @@ public class ApiTest {
         }
         rd.close();
         conn.disconnect();
-
 //        System.out.println(sb);
+        // 문자열 객체로 만들어준다. 네트워크를 통해 들어오는 데이터는 일반 텍스트
         String jsonData = sb.toString();
 
+        // 필요데이터만을 처리하기 위한 파싱 작업
         JsonElement jsonElement = JsonParser.parseString(jsonData);
+        // 자바 json 객체
         JsonObject object = jsonElement.getAsJsonObject();
 
         JsonObject responseObj = object.get("response").getAsJsonObject();
@@ -65,25 +71,25 @@ public class ApiTest {
 //        System.out.println(arrayData);
         Gson gson = new Gson();
 
-        System.out.println(arrayData.size());
-        TourInfo tourInfo = gson.fromJson(arrayData.get(0), TourInfo.class);
+//        System.out.println(arrayData.size());
+//        TourInfo tourInfo = gson.fromJson(arrayData.get(0), TourInfo.class);
 
-        System.out.println(tourInfo.getCourseName());
-        System.out.println(tourInfo.getSpotAreaName());
+//        System.out.println(tourInfo.getCourseName());
+//        System.out.println(tourInfo.getSpotAreaName());
 
-        ArrayList<TourInfo> tourInfos = new ArrayList<TourInfo>();
+        List<TourInfo> tourInfos = new ArrayList<TourInfo>();
 
         //https://www.jsonschema2pojo.org/   ----> json class 변환
         for (int i = 0; i < arrayData.size(); i++) {
-            tourInfo = gson.fromJson(arrayData.get(i), TourInfo.class);
+            TourInfo tourInfo = gson.fromJson(arrayData.get(i), TourInfo.class);
             tourInfos.add(tourInfo);
         }
 
-        for (TourInfo tInfo : tourInfos) {
-            System.out.println(tInfo.getCourseName());
-            System.out.println(tInfo.getThema());
-        }
-
+//        for (TourInfo tInfo : tourInfos) {
+//            System.out.println(tInfo.getCourseName());
+//            System.out.println(tInfo.getThema());
+//        }
+        model.addAttribute("tourInfos", tourInfos);
 
 //        sb.toString();
 //        Gson gson = new GsonBuilder().create();
@@ -91,5 +97,7 @@ public class ApiTest {
 //        DaejeonTourList dinfo = gson.fromJson(sb.toString(), DaejeonTourList.class);
 //        return dinfo; // 객체 리턴
 //        System.out.println(sb.toString());
+
+        return "test";
     }
 }
